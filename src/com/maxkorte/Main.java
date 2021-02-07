8e7eeadb5b76bbcb46916ac3b22e3d6c9c66ac80
Max Korte <privat@max-korte.de>
2021-02-07 13:31:04 +0100

    implemented functionality

--- src/com/maxkorte/Main.java
+++ src/com/maxkorte/Main.java
@@ -3,6 +3,23 @@ package com.maxkorte;
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+	    byte height = Byte.parseByte(args[0]);
+        printDiamond(height);
+    }
+
+    public static void printDiamond(byte height){
+        for (byte i = 0; i < height; i++){
+            printStep(i);
+        }
+        for (byte i = (byte) (height - 2); i >= 0; i--){
+            printStep(i);
+        }
+    }
+
+    public static void printStep(byte i){
+        for (byte j = 0; j <= i; j++){
+            System.out.print("*");
+        }
+        System.out.println();
     }
 }
